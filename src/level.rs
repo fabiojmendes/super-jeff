925a5eaf416a90d4555a03f5591df34e519070ab
Fabio Mendes <fabiojmendes@gmail.com>
2021-04-06 18:32:41 -0400

    Raging around

--- src/level.rs
+++ src/level.rs
@@ -18,11 +18,13 @@ pub struct Monkey {
     timer: Instant,
     pub enranged: bool,
     rage_velocity: Vec2,
+    rage_start: Vec2,
 }
 
 impl Monkey {
-    const RAGE_START: Duration = Duration::from_millis(500);
-    const RAGE_DURATION: Duration = Duration::from_millis(2300);
+    const RAGE_DELAY: Duration = Duration::from_millis(750);
+    const RAGE_MAX_DISTANCE: f32 = 27.0;
+    const BANANA_MAX_DISTANCE: f32 = 30.0;
 
     fn new() -> Monkey {
         Monkey {
@@ -34,25 +36,26 @@ impl Monkey {
             timer: Instant::now(),
             enranged: false,
             rage_velocity: Vec2::new(-15.0, 0.0),
+            rage_start: Vec2::ZERO,
         }
     }
 
     fn rage(&mut self) {
         self.bananas_thrown = 0;
         self.enranged = true;
-        println!("RAGE!");
+        self.rage_start = self.position;
     }
 
     fn throw_banana(&mut self, target: Vec2) {
-        let distance = target - self.position;
-        if distance.x.abs() > 30.0 {
+        let displacement = target - self.position;
+        if displacement.x.abs() > Monkey::BANANA_MAX_DISTANCE {
             return;
         }
         // Random y velocity based on the distance from the target
-        let yvel = (rand::random::<f32>() * 7.5) + (distance.x.abs() / 2.0);
+        let yvel = (rand::random::<f32>() * 7.5) + (displacement.x.abs() / 2.0);
         // Calculate the trajectory based on the random y velocity and distance from target
         // https://www.dummies.com/education/science/physics/calculate-the-range-of-a-projectile-fired-at-an-angle/
-        let velocity = Vec2::new((distance.x * -physics::GRAVITY.y / yvel) / 2.0, yvel);
+        let velocity = Vec2::new((displacement.x * -physics::GRAVITY.y / yvel) / 2.0, yvel);
         self.bananas.push(Banana { position: self.position, sides: Vec2::new(0.5, 0.3), velocity });
         self.bananas_thrown += 1;
     }
@@ -60,19 +63,18 @@ impl Monkey {
     fn udpate(&mut self, elapsed: f32, target: Vec2, level_bounds: Vec2) {
         let mut rng = rand::thread_rng();
         if self.enranged {
-            if self.timer.elapsed() >= Monkey::RAGE_START {
+            if self.timer.elapsed() >= Monkey::RAGE_DELAY {
                 self.velocity = self.rage_velocity;
             }
-            if self.timer.elapsed() >= Monkey::RAGE_DURATION {
+            if self.position.distance(self.rage_start) >= Monkey::RAGE_MAX_DISTANCE {
                 self.timer += self.timer.elapsed();
                 self.enranged = false;
                 self.velocity = Vec2::ZERO;
                 self.rage_velocity = -self.rage_velocity;
-                println!("End Rage");
             }
         } else if self.bananas_thrown >= rng.gen_range(5..10) {
             self.rage();
-        } else if self.timer.elapsed() > Duration::from_millis(rng.gen_range(500..1500)) {
+        } else if self.timer.elapsed() > Duration::from_millis(rng.gen_range(1000..2000)) {
             self.timer += self.timer.elapsed();
             self.throw_banana(target);
         }
