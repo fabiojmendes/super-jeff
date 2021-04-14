1a70057e9a4b735a141094647888f6fc687a5634
Fabio Mendes <fabiojmendes@gmail.com>
2021-04-14 18:58:54 -0400

    Fix travis.yml

--- deploy.rb
+++ deploy.rb
@@ -1,1 +1,9 @@
+require 'octokit'
+
 puts "deploy"
+
+# Set access_token instead of login and password if you use personal access token
+client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+
+# Fetch the current user
+puts client.user
