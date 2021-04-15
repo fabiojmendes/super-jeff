a124602f069e049955e4e5e828dcf09b7624dc21
Fabio Mendes <fabiojmendes@users.noreply.github.com>
2021-04-14 22:55:20 -0400

    Delete deploy.rb

--- deploy.rb
+++
@@ -1,9 +0,0 @@
-require 'octokit'
-
-puts "Custom deploy script running"
-
-# Set access_token instead of login and password if you use personal access token
-client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
-
-# Fetch the current user
-puts client.user
