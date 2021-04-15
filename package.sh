6a2ffab90b140a9ba6c855bad6d43caa306706b0
Fabio Mendes <fabiojmendes@gmail.com>
2021-04-14 20:30:21 -0400

    Package Script

---
+++ package.sh
@@ -0,0 +1,24 @@
+echo "Running packaging script"
+FILE_NAME="super-jeff-$TRAVIS_TAG-$TRAVIS_OS_NAME"
+
+echo "Packaging $FILE_NAME"
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+  ARTIFACT="$FILE_NAME.zip"
+  mkdir super-jeff
+  cp target/release/super-jeff.exe ./super-jeff
+  cp -r assets ./super-jeff
+  cp lib/*.{dll,txt} ./super-jeff
+  ls -l super-jeff
+  tar -cavf $ARTIFACT super-jeff
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  tar -C target/release -cvf $FILE_NAME.tar super-jeff
+  tar -rvf $FILE_NAME.tar assets
+  gzip -f $FILE_NAME.tar
+  ARTIFACT="$FILE_NAME.tar.gz"
+else
+  echo "OS not identified"
+  exit 1
+fi
+
+echo "$ARTIFACT generated"
